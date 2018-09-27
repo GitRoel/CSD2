43c67f6058fb2506d97ed2d61f4dad0819b349e1
Roel van der Meulen <roel.vandermeulen@outlook.com>
2018-09-27 15:24:46 +0200

    Added final version opdracht2

--- python_basics/opdracht2.py
+++ python_basics/opdracht2.py
@@ -1,25 +1,30 @@
 import simpleaudio as sa
 import time
 
+# creating an empty list for the rhythm forloop
+rhytmList = []
 
-print("how many tiem?")
+print("how many samples?")
 numPlaybackTimes = input("")
-print(numPlaybackTimes)
 
-print("welk ritme")
+# import snare sample
+wave_obj = sa.WaveObject.from_wave_file("audio_files/rim_short.wav")
 
-#ritme1 = float(ritme)
+print("which rhythm")
 
-ritme = [float(x) for x in input().split()]
-print(ritme)
+# creating "numPlaybackTimes" amount of inputs and appending those inputs to create a list
+for rhythm in range(0, int(numPlaybackTimes)):
+    tijd = input()
+    rhytmList.append(tijd)
+print(rhytmList)
 
-for times in range(0, int(numPlaybackTimes)):
-    for play in ritme:
-        wave_obj = sa.WaveObject.from_wave_file("audio_files/rim_short.wav")
-        play_obj = wave_obj.play()
-        play_obj.wait_done()
-        print(play)
-        for time in ritme:
-            time.sleep(ritme)
-    print("end loop")
-    print()
+print("which BPM?")
+bpm = input()
+aantalSecondeperbeat = 60/int(bpm);
+print(aantalSecondeperbeat)
+
+# plays the sample with the delay-values from someList
+for x in rhytmList:
+    print(x)
+    play_obj = wave_obj.play()
+    time.sleep(aantalSecondeperbeat * float(x))
